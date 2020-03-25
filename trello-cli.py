34c3e0c5a4702bc8a5fdfc483eff348d286c6bad
Matthieu Clemenceau <matthieu.clemenceau@canonical.com>
2020-03-25 15:35:01 -0500

    Specialized the code to only return open active cards per members and lanes

--- trello-cli.py
+++ trello-cli.py
@@ -22,65 +22,37 @@ def trelloCLi():
         return 1
 
     client = TrelloClient(api_key=config['api_key'],token=config['token'])
-
-    # List Boards Available to user
-    for board in client.list_boards():
-        print(board.name)
-
-    print("---")
-    # Pick a Board (Frankfurt and print all the lanes)
-    the_board = client.get_board(FRANKFURT_RETRO_2020_03)
-    for lane in the_board.get_lists('all'):
-        print(lane.name)
-
-    print("---")
-    # Pick a Board (The Board and print only active lanes)
     the_board_lanes = {}
+    the_board_members = {}
+    the_board_cards = []
+
+    # Pick The Board Open Lanes
     the_board = client.get_board(THE_BOARD_ID)
     for lane in the_board.get_lists('open'):
         the_board_lanes[lane.name] = lane.id
-        print(lane.name)
-
-    print(the_board_lanes)
 
     # Capture the board members in a dictionary
-    the_board_members = {}
     for member in the_board.get_members():
         the_board_members[member.full_name] = member.id
         
-        
-    print("---")
-    # Print all the Cards from a specific lane (In Progress)
-    the_board = client.get_board(THE_BOARD_ID)
-    the_board_cards = the_board.open_cards()
-    for card in the_board_cards:
-        if card.list_id == the_board_lanes['In Progress']:
-            print (card.name)
+    # Capture all the cards from active lane into a dictionary
+    for card in the_board.open_cards():
+        if card.list_id in the_board_lanes.values():
+            the_board_cards.append(card)
 
-    print("---")
-
-    # Print all the Cards from a specific lane Assigned to Tiago
-    the_board = client.get_board(THE_BOARD_ID)
-    the_board_cards = the_board.open_cards()
-    for card in the_board_cards:
-        if card.list_id == the_board_lanes['In Progress']:
-            if the_board_members['Tiago Stürmer Daitx'] in card.member_id:
-                print (card.name)
-
-    # for board in board_available.items():
-    #     print(board)
-
-
-    # the_board_cards = the_board.get_cards()
-
-    # the_board_members = the_board.get_members()
-   
-
-    # Find all Cards with Member Matthieu
-    # For each list
-    # If list is not Archived
-    # For each card
-    # If Card members include Matthieu
-    # Print Card
+    # Print active cards per member/lane
+    for member in the_board_members:
+        if len(list(filter(lambda x: the_board_members[member] in x.member_id,the_board_cards)))>0:
+            print(" ")
+            print(member)
+            for lane in the_board_lanes:
+                card_list = []    
+                for card in list(filter(lambda x: the_board_members[member] in x.member_id and 
+                                                x.list_id == the_board_lanes[lane],the_board_cards)):
+                    card_list.append(card)
+                if len(card_list) > 0:
+                    print("  {}".format(lane))
+                    for c in card_list:
+                        print("    * {}".format(c.name))
 
 trelloCLi()
