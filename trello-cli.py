b7d0e3dc3b89ec24e2c91d381657e84ee8d89100
Matthieu Clemenceau <matthieu.clemenceau@canonical.com>
2020-03-26 08:32:36 -0500

    Added command line member selection

--- trello-cli.py
+++ trello-cli.py
@@ -1,24 +1,37 @@
+#!/usr/bin/python3
 import sys
 import os
 import json
 
 from trello import TrelloClient
+from optparse import OptionParser
 
 THE_BOARD_ID='hUIl3E3I'
 FRANKFURT_RETRO_2020_03='63fehZ0X'
 
-def trelloCLi():
+def main():
 
-    #TODO Use Command parser
-    #TODO Set the config file in more location and use $HOME env value
 
-    config_file = '/home/mclemenceau/.trello.creds'
+    # config_file = '/home/mclemenceau/.trello.creds'
     
+    parser = OptionParser(
+        usage='Usage: %prog [options]')
+
+    #TODO Set the config file in more location and use $HOME env value
+    default_config=os.path.expanduser('~')
+    parser.add_option(
+         '-c', dest='credentials',help='location of the trello credential json file',
+         default="{}/.trello.creds".format(default_config))
+    parser.add_option(
+        '-m', dest='member',help='filter with a specific team member', default='all')
+
+    opts, args = parser.parse_args()
+
     try:
-        with open(config_file) as f:
+        with open(opts.credentials) as f:
             config = json.load(f)
     except (FileNotFoundError, json.JSONDecodeError):
-        print('Credentials file {} could not be parsed.'.format(config_file))
+        print('Credentials file {} could not be parsed.'.format(opts.credentials))
         return 1
 
     client = TrelloClient(api_key=config['api_key'],token=config['token'])
@@ -34,7 +47,14 @@ def trelloCLi():
     # Capture the board members in a dictionary
     for member in the_board.get_members():
         the_board_members[member.full_name] = member.id
-        
+    
+    poi=opts.member
+    if len(poi) > 0 and poi != 'all':
+        for name in the_board_members.keys():
+            if poi in name:
+                poi = name
+                break   
+
     # Capture all the cards from active lane into a dictionary
     for card in the_board.open_cards():
         if card.list_id in the_board_lanes.values():
@@ -42,17 +62,19 @@ def trelloCLi():
 
     # Print active cards per member/lane
     for member in the_board_members:
-        if len(list(filter(lambda x: the_board_members[member] in x.member_id,the_board_cards)))>0:
-            print(" ")
-            print(member)
-            for lane in the_board_lanes:
-                card_list = []    
-                for card in list(filter(lambda x: the_board_members[member] in x.member_id and 
-                                                x.list_id == the_board_lanes[lane],the_board_cards)):
-                    card_list.append(card)
-                if len(card_list) > 0:
-                    print("  {}".format(lane))
-                    for c in card_list:
-                        print("    * {}".format(c.name))
+        if poi == "all" or poi == member:
+            if len(list(filter(lambda x: the_board_members[member] in x.member_id,the_board_cards)))>0:
+                print(member)
+                for lane in the_board_lanes:
+                    card_list = []    
+                    for card in list(filter(lambda x: the_board_members[member] in x.member_id and 
+                                                    x.list_id == the_board_lanes[lane],the_board_cards)):
+                        card_list.append(card)
+                    if len(card_list) > 0:
+                        print("  {}".format(lane))
+                        for c in card_list:
+                            print("    * {}".format(c.name))
+                print(" ")
 
-trelloCLi()
+if __name__ == "__main__":
+    main()
