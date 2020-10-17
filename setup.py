cde21e6e50b365492b4d2382bf34b64ee32d3633
Matthieu Clemenceau <matthieu.clemenceau@canonical.com>
2020-10-17 12:28:57 -0500

    pep8 cleanup and packaging improvements

--- setup.py
+++ setup.py
@@ -1,24 +1,34 @@
-import os
+import setuptools
+
 from setuptools import setup
 
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
-    name = "TrelloCli",
-    version = "0.0.3",
-    author = "Matthieu Clemenceau",
-    author_email = "matthieu.clemenceau@canonical.com",
-    description = ("A Command Line helper to access Trello Data."),
-    license = "GPLv3",
-    keywords = "",
-    url = "https://github.com/mclemenceau/trello-cli",
-    packages=['TrelloCli'],
-    scripts=["bin/trello-cli"],
-    long_description=read('README.md'),
+    name="TrelloCli",
+    version="0.0.4",
+    author="Matthieu Clemenceau",
+    author_email="matthieu.clemenceau@canonical.com",
+    description=("A Command Line helper to access Trello Data."),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mclemenceau/trello-cli",
+    project_urls={
+        'Bug Reports': 'https://github.com/mclemenceau/trello-cli/issues',
+        'Source': 'https://github.com/mclemenceau/trello-cli/',
+    },
+    packages=setuptools.find_packages(),
+    keywords='trello cli',
+    entry_points={
+        'console_scripts': [
+            'trello-cli=TrelloCli.main:main',
+        ],
+    },
+    install_requires=['py-trello'],
     classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Topic :: Utilities",
-        "License :: OSI Approved :: GPLv3 License",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
     ],
 )
--- trello-cli.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-#
-# Mostly needed for local testing and debugging
-
-from TrelloCli.main import *
-
-main()
